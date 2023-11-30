29e466aed7649943adb86135bbb10d6db12506f6
Yamato2434 <yamato2434@gmail.com>
2023-11-30 09:46:41 +0900

    できてるかわかんない

---
+++ MyGitProject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import java.util.Random;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+//		kadaiA();
+		kadaiB();
+	}
+
+		static void kadaiB() {
+			Random rand = new Random();
+		    int num = rand.nextInt(100) + 1;
+		    System.out.println(num);
+	}
+}
